4fc12b9724ad496657f0ea9fe3dad484d6da1fbc
Chrahuldeveloper <chrahulofficial@gmail.com>
2026-07-09 11:10:45 +0530

    fixed

--- main.py
+++ main.py
@@ -1064,7 +1064,7 @@ origins = [
     "http://localhost:3000",
     "https://www.storio.tech",
     "https://storio.tech",
-    "https://www.storio.tech/ai-youtube-content-generator"
+    "https://www.storio.tech/ai-youtube-content-generator",
     "https://storio.tech/ai-youtube-content-generator"
 ]
 
